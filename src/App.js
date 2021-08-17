76b7deb46947b01351213370b9bbb28a1c2a9200
KeerthiBandi <keerthi.gadipelli@gmail.com>
2021-08-17 11:40:02 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,36 @@
-import logo from './logo.svg';
-import './App.css';
 
-function App() {
+import './App.css';
+import InputField from './components/InputFields';
+import TodosList from './components/TodosList';
+import { connect } from 'react-redux';
+import { deleteAll, persistTodos } from './redux/actions/addTodo';
+import { useEffect } from 'react';
+
+const App = ({ deleteAll, persistTodos }) => {
+  useEffect(() => {
+    persistTodos();
+  },[persistTodos])
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="todosdiv">
+      <h1 className="todoh1">Todo List</h1>
+      <InputField />
+      <TodosList />
+      <div>
+        <button className ="deleteallbutton" onClick={()=>deleteAll()}>
+          {" "}
+          Delete All
+        </button>
+      </div>
     </div>
-  );
+  )
 }
 
-export default App;
+const mapDispatchToProps = dispatch => ({
+  deleteAll: () => dispatch(deleteAll()),
+  persistTodos: () => dispatch(persistTodos())
+});
+
+export default connect(
+  null,
+  mapDispatchToProps
+  )(App);
